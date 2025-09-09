61bc391a5cea31b115e47fe704527cd10fc5eb9e
Jatin-6622 <jatin7814226168@gmail.com>
2025-09-09 14:27:58 +0530

    Addon Commit

---
+++ MoneyControl_CapstoneProject/src/test/java/runners/ParallelRun.java
@@ -0,0 +1,28 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"stepdefinitions","hooks"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports.html",
+        "json:target/cucumber.json"
+    },
+    monochrome = true,
+    tags = "@positive"
+)
+public class ParallelRun extends AbstractTestNGCucumberTests {
+
+    // Enable parallel execution
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
+
+//******************** Under Implementation plese dont use ***************************//
